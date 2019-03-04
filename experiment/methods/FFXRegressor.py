50b7053c6526bbf8e0e7f7463958352afa7fe87c
James McDermott <jamesmichaelmcdermott@gmail.com>
2019-03-04 15:28:00 +0000

    Added FFXRegressor.

---
+++ experiment/methods/FFXRegressor.py
@@ -0,0 +1,12 @@
+import metrics
+from ffx import FFXRegressor
+
+hyper_params = [
+    {
+        # TODO: we could define a hyperparameter to tune how we choose
+        # from the Pareto front. For now, FFXRegressor will just return
+        # the most complex (= most accurate).
+    },
+]
+
+est = FFXRegressor()
