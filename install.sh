7f173ebea7bbd5188d8f9da9d39a24d01ed08520
lacava <williamlacava@gmail.com>
2020-12-23 12:02:50 -0500

    going back to install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,22 @@ conda activate regression-benchmarks
 conda info 
 
 # install methods
+echo "///////////////////////////"
 echo "installing GP methods..."
+echo "///////////////////////////"
 
 # move to methods folder
 cd experiment/methods/src/
 
 # install all methods
 for install_file in $(ls *.sh) ; do
+    echo "///////////////////////////"
+    echo "Running $install_file"
+    echo "///////////////////////////"
+
     bash $install_file
+
+    echo "///////////////////////////"
+    echo "Finished $install_file"
+    echo "///////////////////////////"
 done
-	# bash $install_file
