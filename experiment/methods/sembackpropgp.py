2032eeb11f84941224ff598a593645d5720541ae
Marco <marcovirgolin@users.noreply.github.com>
2021-04-29 17:44:59 +0200

    Update sembackpropgp.py

--- experiment/methods/sembackpropgp.py
+++ experiment/methods/sembackpropgp.py
@@ -2,37 +2,38 @@ from pyGPGOMEA import GPGOMEARegressor as GPG
 
 hyper_params = [
     {
-        'popsize': (100,), 'generations': (1000,),
+        'popsize': (100,), 'functions' : ('+_-_*_aq',), 'linearscaling' : (True,),
+        'sbrdo': (1.0,), 'submut': (0.0,), 
+    },
+    {
+        'popsize': (1000,), 'functions' : ('+_-_*_aq',), 'linearscaling' : (True,),
         'sbrdo': (1.0,), 'submut': (0.0,),
     },
     {
-        'popsize': (1000,), 'generations': (100,),
-        'sbrdo': (1.0,), 'submut': (0.0,),
-    },
-    {
-        'popsize': (500,), 'generations': (200,),
-        'sbrdo': (1.0,), 'submut': (0.0,),
-    },
-    {
-        'popsize': (100,), 'generations': (1000,),
+        'popsize': (100,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
         'sbrdo': (0.75,), 'submut': (0.25,),
     },
     {
-        'popsize': (1000,), 'generations': (100,),
+        'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
         'sbrdo': (0.75,), 'submut': (0.25,),
     },
     {
-        'popsize': (500,), 'generations': (200,),
+        'popsize': (100,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (False,),
         'sbrdo': (0.75,), 'submut': (0.25,),
     },
+    {
+        'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (False,),
+        'sbrdo': (0.75,), 'submut': (0.25,),
+    },
+
 ]
 
-est = GPG( popsize=1000, generations=100, time=-1, evaluations=-1, 
+est = GPG( popsize=100, generations=-1, time=-1, evaluations=500000, 
     linearscaling=True, functions='+_-_*_aq', erc=True, initmaxtreeheight=6, 
     maxtreeheight=15, maxsize=1000, 
     subcross=0.0, sbagx=False,
     sbrdo=0.75, submut=0.25,
-    sblibtype='p_15_9999_l_n',
+    sblibtype='p_10_9999_l_n',
     unifdepthvar=True, 
     tournament=4,
     caching=False,
