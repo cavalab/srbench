9b66b2a75b62f696e8684d6123071bf35dbe274b
Marco <marcovirgolin@users.noreply.github.com>
2021-04-29 18:28:27 +0200

    Update sembackpropgp.py

--- experiment/methods/sembackpropgp.py
+++ experiment/methods/sembackpropgp.py
@@ -2,34 +2,35 @@ from pyGPGOMEA import GPGOMEARegressor as GPG
 
 hyper_params = [
     {
-        'popsize': (100,), 'functions' : ('+_-_*_aq',), 'linearscaling' : (True,),
-        'sbrdo': (1.0,), 'submut': (0.0,), 
+        'popsize': (1000,), 'functions' : ('+_-_*_aq',), 'linearscaling' : (True,),
+        'sbrdo': (1.0,), 'submut': (0.0,), 'tournament': (4,),
     },
     {
         'popsize': (1000,), 'functions' : ('+_-_*_aq',), 'linearscaling' : (True,),
-        'sbrdo': (1.0,), 'submut': (0.0,),
-    },
-    {
-        'popsize': (100,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
-        'sbrdo': (0.75,), 'submut': (0.25,),
+        'sbrdo': (1.0,), 'submut': (0.0,), 'tournament': (8,),
     },
     {
         'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
-        'sbrdo': (0.75,), 'submut': (0.25,),
+        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (4,),
+    },
+    {
+        'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
+        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (8,),
     },
     {
         'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (False,),
-        'sbrdo': (0.75,), 'submut': (0.25,),
+        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (4,),
     },
     {
-        'popsize': (5000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (False,),
-        'sbrdo': (0.75,), 'submut': (0.25,),
+        'popsize': (10000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (False,),
+        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (8,),
     },
 ]
 
+# Note: max size and max tree height are TBD I guess.
 est = GPG( popsize=100, generations=-1, time=-1, evaluations=500000, 
     linearscaling=True, functions='+_-_*_aq', erc=True, initmaxtreeheight=6, 
-    maxtreeheight=15, maxsize=1000, 
+    maxtreeheight=20, maxsize=5000, 
     subcross=0.0, sbagx=False,
     sbrdo=0.75, submut=0.25,
     sblibtype='p_10_9999_l_n',
