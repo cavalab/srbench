f5152f2782c742a2eb7209732b9b9b0f404ed55a
lacava <williamlacava@gmail.com>
2021-04-28 23:39:05 -0400

    renamed AFP, eplex, adds fitness estimating version

---
+++ experiment/methods/AFPRegressor.py
@@ -0,0 +1,44 @@
+from ellyn import ellyn
+
+# 500,000 evaluations = 250,000 with 1 constant hill climbing iteration
+pop_sizes = [100, 500, 1000]
+gs = [2500, 500, 250]
+op_lists=[
+        ['n','v','+','-','*','/','sin','cos','exp','log','2','3', 'sqrt'],
+        ['n','v','+','-','*','/', 'exp','log','2','3', 'sqrt']
+        ]
+
+hyper_params = []
+
+for p, g in zip(pop_sizes, gs):
+    for op_list in op_lists:
+        hyper_params.append({
+                'popsize':[p],
+                'g':[g],
+                'op_list':[op_list]
+                })
+
+
+# Create the pipeline for the model
+est = ellyn(selection='afp',
+            lex_eps_global=False,
+            lex_eps_dynamic=False,
+            islands=True,
+            num_islands=1,
+            island_gens=1,
+            verbosity=1,
+            print_data=False,
+            elitism=True,
+            pHC_on=True,
+            prto_arch_on=True,
+            max_len = 64,
+            max_len_init=20,
+            op_list=['n','v','+','-','*','/','sin','cos','exp','log','2','3',
+                     'sqrt'],
+            )
+
+def complexity(est):
+    return len(est.best_estimator_)
+
+def model(est):
+    return est.stack_2_eqn(est.best_estimator_)
