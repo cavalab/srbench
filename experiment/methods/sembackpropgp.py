e7b866d752a3208282ed6953b9f402466dd66688
Marco <marcovirgolin@users.noreply.github.com>
2021-05-03 09:03:24 +0200

    Update sembackpropgp.py

--- experiment/methods/sembackpropgp.py
+++ experiment/methods/sembackpropgp.py
@@ -2,35 +2,35 @@ from pyGPGOMEA import GPGOMEARegressor as GPG
 
 hyper_params = [
     {
-        'popsize': (1000,), 'functions' : ('+_-_*_aq',), 'linearscaling' : (True,),
-        'sbrdo': (1.0,), 'submut': (0.0,), 'tournament': (4,),
-    },
-    {
-        'popsize': (1000,), 'functions' : ('+_-_*_aq',), 'linearscaling' : (True,),
-        'sbrdo': (1.0,), 'submut': (0.0,), 'tournament': (8,),
+        'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
+        'sbrdo': (0.9,), 'submut': (0.1,), 'tournament': (4,), 'maxsize': (1000,),
     },
     {
         'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
-        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (4,),
+        'sbrdo': (0.9,), 'submut': (0.1,), 'tournament': (8,), 'maxsize': (1000,),
     },
     {
         'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
-        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (8,),
+        'sbrdo': (0.9,), 'submut': (0.1,), 'tournament': (4,), 'maxsize': (5000,),
+    },
+    {
+        'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (True,),
+        'sbrdo': (0.9,), 'submut': (0.1,), 'tournament': (8,), 'maxsize': (5000,),
     },
     {
         'popsize': (1000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (False,),
-        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (4,),
+        'sbrdo': (0.9,), 'submut': (0.1,), 'tournament': (4,), 'maxsize': (250,), 'sblibtype': ('p_6_20000',), 
     },
     {
         'popsize': (10000,), 'functions' : ('+_-_*_aq_plog_sin_cos',), 'linearscaling' : (False,),
-        'sbrdo': (0.75,), 'submut': (0.25,), 'tournament': (8,),
+        'sbrdo': (0.9,), 'submut': (0.1,), 'tournament': (8,), 'maxsize': (250,), 'sblibtype': ('p_6_20000',),
     },
 ]
 
 # Note: max size and max tree height are TBD I guess.
 est = GPG( popsize=100, generations=-1, time=-1, evaluations=500000, 
-    linearscaling=True, functions='+_-_*_aq', erc=True, initmaxtreeheight=6, 
-    maxtreeheight=20, maxsize=5000, 
+    linearscaling=True, functions='+_-_*_aq_plog_sin_cos', erc=True, initmaxtreeheight=6, 
+    maxtreeheight=20, maxsize=1000, 
     subcross=0.0, sbagx=False,
     sbrdo=0.75, submut=0.25,
     sblibtype='p_10_9999_l_n',
