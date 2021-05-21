4d8adf3587b7159cbbe3154e0a83010478f9a0ca
lacava <williamlacava@gmail.com>
2021-05-21 01:06:52 -0400

    redoubles FE evals with time limit

--- experiment/methods/tuned/FE_AFPRegressor.py
+++ experiment/methods/tuned/FE_AFPRegressor.py
@@ -4,6 +4,6 @@ from .params._fe_afpregressor import params
 est.set_params(**params)
 est.op_list = ['n','v','+','-','*','/','exp','log','2','3','sqrt','sin','cos']
 est.time_limit = 8*60*60
-# doubling evals takes too long, leave as is
-# est.g = int(est.g*2**0.5)
-# est.popsize = int(est.popsize*2**0.5)
+# doubling evals 
+est.g = int(est.g*2**0.5)
+est.popsize = int(est.popsize*2**0.5)
