3dbe42239c26d971fa75eff773212805a72b720b
zhenlingcn <zhenlingcn@foxmail.com>
2021-07-03 19:05:36 +0800

    fix errors in installation

--- experiment/methods/src/pstree_install.sh
+++ experiment/methods/src/pstree_install.sh
@@ -1,11 +1,12 @@
 #install PS Tree
 
 # remove directory if it exists
-if [ -d "PS_Tree" ]; then
-    rm -rf PS_Tree
+if [ -d "PS-Tree" ]; then
+    rm -rf PS-Tree
 fi
 
 git clone https://github.com/zhenlingcn/PS-Tree
 
-cd PS_Tree
+cd PS-Tree
+pip install -r requirements.txt
 python setup.py install
