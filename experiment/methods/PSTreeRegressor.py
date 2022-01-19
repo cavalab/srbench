65efd8616641c4d0e910b87f1fca2a30d2303aa5
Hengzhe Zhang <2970057811@qq.com>
2022-01-19 22:04:33 +0800

    using TournamentDCD as the default selection operator

--- experiment/methods/PSTreeRegressor.py
+++ experiment/methods/PSTreeRegressor.py
@@ -1,4 +1,4 @@
-from pstree.cluster_gp_sklearn import GPRegressor, PSTreeRegressor
+from pstree.cluster_gp_sklearn import GPRegressor, PSTreeRegressor, selTournamentDCD
 from pstree.complexity_utils import tree_gp_regressor_complexity
 from sklearn.tree import DecisionTreeRegressor
 
@@ -9,7 +9,8 @@ hyper_params = [
 
 est = PSTreeRegressor(regr_class=GPRegressor, tree_class=DecisionTreeRegressor,
                       height_limit=6, n_pop=25, n_gen=500,
-                      basic_primitive=False, size_objective=True)
+                      basic_primitive=False, size_objective=True,
+                      select=selTournamentDCD)
 
 
 def complexity(est):
