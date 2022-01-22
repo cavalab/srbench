28b4ac3ac30a80856d0d3dc243442df1a5fa2466
MilesCranmer <miles.cranmer@gmail.com>
2022-01-22 17:28:05 -0500

    Reduce PySR hyperparams for quick testing

--- experiment/methods/PySRRegressor.py
+++ experiment/methods/PySRRegressor.py
@@ -13,7 +13,7 @@ class PySRRegressor(BaseEstimator, RegressorMixin):
         model_selection="accuracy",
         binary_operators=None,
         unary_operators=None,
-        populations=40,
+        populations=20,
         niterations=20,
         ncyclesperiteration=300,
         annealing=False,
@@ -121,19 +121,22 @@ est = PySRRegressor()
 poly_basis = ["square(x) = x^2", "cube(x) = x^3", "quart(x) = x^4"]
 trig_basis = ["cos", "sin"]
 exp_basis = ["exp", "log", "sqrt"]
+# Hyperparams are reduced for speed of testing:
 hyper_params = [
     {
-        "annealing": (True, False),
-        "denoise": (True, False),
+        "annealing": (True,), # (True, False)
+        "denoise": (True,), # (True, False)
         "binary_operators": (["+", "-", "*", "/"],),
         "unary_operators": (
             [],
-            poly_basis,
-            poly_basis + trig_basis,
-            poly_basis + exp_basis,
+            # poly_basis,
+            # poly_basis + trig_basis,
+            # poly_basis + exp_basis,
         ),
-        "populations": (40, 80),
-        "alpha": (0.01, 0.1, 1.0, 10.0),
-        "model_selection": ("accuracy", "best"),
+        "populations": (20,), # (40, 80),
+        "alpha": (1.0,),
+        "model_selection": ("best",)
+        # "alpha": (0.01, 0.1, 1.0, 10.0),
+        # "model_selection": ("accuracy", "best"),
     }
 ]
