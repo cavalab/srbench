e7440735681e14c69e766adb41f432b7f916dc13
lacava <williamlacava@gmail.com>
2022-02-23 13:18:05 -0500

    update dsr install to not be editable

--- experiment/methods/src/dsr_install.sh
+++ experiment/methods/src/dsr_install.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
 # install deep-symbolic-regression
 
 if [ -d deep-symbolic-regression ] ; then
     rm -rf deep-symbolic-regression
 fi
 
-git clone --quiet https://github.com/lacava/deep-symbolic-regression 
+git clone https://github.com/lacava/deep-symbolic-regression 
 
 cd deep-symbolic-regression
 
 pip install -r requirements.txt
 # export CFLAGS="-I $(python -c "import numpy; print(numpy.get_include())") $CFLAGS" # Needed on Mac to prevent fatal error: 'numpy/arrayobject.h' file not found
-cd dsr
-pip install . # Install DSR package
+pip install ./dsr # Install DSR package
+
