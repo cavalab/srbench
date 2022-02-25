3cb93cd80dd7e8053206ce1117569ed15042616f
lacava <williamlacava@gmail.com>
2022-02-24 19:50:28 -0500

    try with pip branch of feat

--- experiment/methods/src/feat_install.sh
+++ experiment/methods/src/feat_install.sh
@@ -5,17 +5,17 @@ if [ -d "feat" ]; then
     rm -rf feat
 fi
 
-git clone  https://github.com/cavalab/feat.git
+git clone  https://github.com/cavalab/feat.git -b pip
 cd feat
 
 # fix version
-git checkout 1f9c43b60eb26bb0d7381cb3a4ebd6243f7355dd
+# git checkout 1f9c43b60eb26bb0d7381cb3a4ebd6243f7355dd
 
-./configure
-export SHOGUN_LIB=$CONDA_PREFIX/lib/
-export SHOGUN_DIR=$CONDA_PREFIX/include/
-export EIGEN3_INCLUDE_DIR=$CONDA_PREFIX/include/eigen3/
-./install n
-cp ../build/libfeat_lib.so $CONDA_PREFIX/lib/
-cd python
+# ./configure
+# export SHOGUN_LIB=$CONDA_PREFIX/lib/
+# export SHOGUN_DIR=$CONDA_PREFIX/include/
+# export EIGEN3_INCLUDE_DIR=$CONDA_PREFIX/include/eigen3/
+# ./install n
+# cp ../build/libfeat_lib.so $CONDA_PREFIX/lib/
+# cd python
 python setup.py install
