045fad2cd2b3ef1d570596eb9ab07e1df604f2a0
lacava <williamlacava@gmail.com>
2022-03-08 15:41:38 -0500

    moved example, update storage

--- ci/store_competitor.sh
+++ ci/store_competitor.sh
@@ -1,12 +1,11 @@
 git config --global user.name 'GitHub Action'
 git config --global user.email 'action@github.com'
-git fetch                         # fetch branches
-git checkout Competition2022       # checkout to your branch
-git pull origin Competition2022
+# git fetch                         # fetch branches
+# git checkout Competition2022       # checkout to your branch
+# git pull origin Competition2022
 # git checkout ${GITHUB_REF##*/} -- $SRC_FILE # copy files from the source branch
 # mkdir -p experiment/methods/$SUBMISSION
-mv submission/$SUBMISSION experiment/methods/
 conda env export -n srbench-submission > experiment/methods/$SUBMISSION/environment.lock.yml
-git add experiment/methods/$SUBMISSION
+git add experiment/methods/$SUBMISSION/*
 git diff-index --quiet HEAD ||  git commit -am "Store $SUBMISSION as competitor"  # commit to the repository (ignore if no modification)
-git push origin Competition2022 # push to remote branch
+git push origin ${GITHUB_REF##*/} # push to remote branch
