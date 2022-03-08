2019cf752d6a39add5be9d75572e6cf51be7250d
lacava <williamlacava@gmail.com>
2022-03-08 16:40:59 -0500

    lots of updates to env variables

--- ci/store_competitor.sh
+++ ci/store_competitor.sh
@@ -5,7 +5,7 @@ git config --global user.email 'action@github.com'
 # git pull origin Competition2022
 # git checkout ${GITHUB_REF##*/} -- $SRC_FILE # copy files from the source branch
 # mkdir -p experiment/methods/$SUBMISSION
-conda env export -n srbench-submission > experiment/methods/$SUBMISSION/environment.lock.yml
-git add experiment/methods/$SUBMISSION/*
-git diff-index --quiet HEAD ||  git commit -am "Store $SUBMISSION as competitor"  # commit to the repository (ignore if no modification)
+conda env export -n srbench-submission > experiment/methods/$SUBNAME/environment.lock.yml
+git add experiment/methods/$SUBNAME/*
+git diff-index --quiet HEAD ||  git commit -am "Store $SUBNAME as competitor"  # commit to the repository (ignore if no modification)
 git push origin ${GITHUB_REF##*/} # push to remote branch
