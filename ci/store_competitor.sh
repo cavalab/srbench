a78e53471c9d41611f848e39e5de5272abfdf7ab
lacava <williamlacava@gmail.com>
2022-03-09 10:11:36 -0500

    store

--- ci/store_competitor.sh
+++ ci/store_competitor.sh
@@ -6,6 +6,6 @@ git config --global user.email 'action@github.com'
 # git checkout ${GITHUB_REF##*/} -- $SRC_FILE # copy files from the source branch
 # mkdir -p experiment/methods/$SUBMISSION
 conda env export -n srbench-submission > experiment/methods/$SUBNAME/environment.lock.yml
-git add experiment/methods/$SUBNAME/*
+git add experiment/methods/$SUBNAME/
 git diff-index --quiet HEAD ||  git commit -am "Store $SUBNAME as competitor"  # commit to the repository (ignore if no modification)
 git push origin ${GITHUB_REF##*/} # push to remote branch
