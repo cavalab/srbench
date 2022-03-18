02709fd974f438a8d831cd2aa4ea0f08ef0ee12a
Hengzhe Zhang <2970057811@qq.com>
2022-03-19 02:30:51 +0800

    a novel hyperparameter grid

--- experiment/methods/PSTreeRegressor.py
+++ experiment/methods/PSTreeRegressor.py
@@ -1,11 +1,18 @@
+from deap.tools import selRandom
 from pstree.cluster_gp_sklearn import GPRegressor, PSTreeRegressor, selTournamentDCD
 from pstree.complexity_utils import tree_gp_regressor_complexity
 from sklearn.tree import DecisionTreeRegressor
 
-hyper_params = [
-    {
-    },
-]
+from sklearn.model_selection import ParameterSampler
+
+hyper_params = list(ParameterSampler({
+    'basic_primitive': ['optimal', 'log,sqrt,sin,tanh'],
+    'initial_height': [None, '0-6'],
+    'max_leaf_nodes': [4, 6, 8],
+}, n_iter=6))
+
+for g in hyper_params:
+    g['select'] = selRandom if g['initial_height'] is None else selTournamentDCD
 
 est = PSTreeRegressor(regr_class=GPRegressor, tree_class=DecisionTreeRegressor,
                       height_limit=6, n_pop=25, n_gen=500, normalize=False,
