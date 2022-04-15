2b92aa366e13dc4074b83b1739bd6004ed853473
lacava <williamlacava@gmail.com>
2022-04-15 10:34:12 -0400

    uses different push variable; triggers storage on merge of PR

--- ci/store_competitor.sh
+++ ci/store_competitor.sh
@@ -1,10 +1,6 @@
 git config --global user.name 'GitHub Action'
 git config --global user.email 'action@github.com'
-# git fetch                         # fetch branches
-# git checkout Competition2022       # checkout to your branch
-# git pull origin Competition2022
-# git checkout ${GITHUB_REF##*/} -- $SRC_FILE # copy files from the source branch
-# mkdir -p experiment/methods/$SUBMISSION
 git add experiment/methods/$SUBNAME/
 git diff-index --quiet HEAD ||  git commit -am "Store $SUBNAME as competitor"  # commit to the repository (ignore if no modification)
-git push origin ${GITHUB_REF##*/} # push to remote branch
+echo "pushing to ${GITHUB_BASE_REF}"
+git push origin ${GITHUB_BASE_REF} # push to remote branch
