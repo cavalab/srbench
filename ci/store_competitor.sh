171ba7f2ba97d3ee066cecb2720cc071dcc1cd98
lacava <williamlacava@gmail.com>
2022-04-15 13:45:38 -0400

    remove merge trigger from ci

--- ci/store_competitor.sh
+++ ci/store_competitor.sh
@@ -2,5 +2,5 @@ git config --global user.name 'GitHub Action'
 git config --global user.email 'action@github.com'
 git add experiment/methods/$SUBNAME/
 git diff-index --quiet HEAD ||  git commit -am "Store $SUBNAME as competitor"  # commit to the repository (ignore if no modification)
-echo "pushing to ${GITHUB_BASE_REF}"
-git push origin ${GITHUB_BASE_REF} # push to remote branch
+echo "pushing to ${GITHUB_HEAD_REF}"
+git push origin ${GITHUB_HEAD_REF} # push to remote branch
