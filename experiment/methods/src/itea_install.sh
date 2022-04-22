174ffe6ed10d2574205cbfdd007fd7484afc0065
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2022-04-22 11:20:04 -0300

    env fixed for itea

--- experiment/methods/src/itea_install.sh
+++ experiment/methods/src/itea_install.sh
@@ -18,7 +18,8 @@ git clone https://github.com/folivetti/ITEA.git
 rsync -av ITEA $CONDA_PREFIX/lib/python3.7/site-packages/ --exclude=".git" --exclude="datasets"
 cd ITEA 
 conda activate srbench
-C_INCLUDE_PATH=$CONDA_PREFIX/include LIBRARY_PATH=$CONDA_PREFIX/lib cabal build 
-LD_LIBRARY_PATH=$CONDA_PREFIX/lib cabal run 
+C_INCLUDE_PATH=$CONDA_PREFIX/include LIBRARY_PATH=$CONDA_PREFIX/lib cabal install
+cp ~/.cabal/bin/itea bin/itea 
+LD_LIBRARY_PATH=$CONDA_PREFIX/lib bin/itea
 
 #cp $CONDA_PREFIX/lib/libgsl.so bin/libgsl.so.0
