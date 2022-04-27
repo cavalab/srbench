80a6c413f559afa96409cbc87369946172d0d5ba
janoPig <jan.pigos@gmail.com>
2022-04-27 09:16:48 +0200

    Hroch - Competition 2022

---
+++ submission/HROCH/install.sh
@@ -0,0 +1,1 @@
+pip install git+https://github.com/janoPig/HROCH.git@d76dbf66438eeba91b32a0a37a822cfd1fea7e3e
---
+++ submission/HROCH/regressor.py
@@ -0,0 +1,43 @@
+from HROCH import Hroch
+import os
+
+est = Hroch()
+
+
+def model(est, X=None):
+    if X is None or not hasattr(X, 'columns'):
+        return est.sexpr
+    model_str = est.sexpr
+    mapping = {'x_'+str(i): k for i, k in enumerate(X.columns)}
+    new_model = model_str
+    for k, v in reversed(mapping.items()):
+        new_model = new_model.replace(k, v)
+
+    return new_model
+
+
+def my_pre_train_fn(est, X, y):
+    # Competition 2022
+    try:
+        est.numThreads = os.environ['OMP_NUM_THREADS']
+    except Exception as e:
+        print(e)
+
+    if len(X) <= 1000:
+        max_time = 360 - 15  # 15 second of slack
+    else:
+        max_time = 3600 - 15  # 15 second of slack
+    est.timeLimit = max_time * 1000
+
+    print(
+        f'my_pre_train_fn: timeLimit={est.timeLimit} numThreads={est.numThreads}')
+    # end Competition 2022
+
+
+# define eval_kwargs.
+eval_kwargs = dict(
+    pre_train=my_pre_train_fn,
+    scale_x=False,  # avoid poisonous scaling
+    scale_y=False,
+    test_params={}
+)
