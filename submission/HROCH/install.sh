3e3e607413255338fd9de8f64757d21fe29db5e0
janoPig <jan.pigos@gmail.com>
2022-04-29 21:57:22 +0200

    try reduce overfited small samples

--- submission/HROCH/install.sh
+++ submission/HROCH/install.sh
@@ -1,1 +1,1 @@
-pip install git+https://github.com/janoPig/HROCH.git@6bc57e8f6c6c11da105d5546d40048ce5d659a3b
+pip install git+https://github.com/janoPig/HROCH.git@504dce17134cbb7ba95f53e30f48ed23835fe483
