7f0b675d427ded91cbdf2b31a95ab0d8c48bb67a
janoPig <jan.pigos@gmail.com>
2022-05-02 06:47:28 +0200

    update

--- submission/HROCH/install.sh
+++ submission/HROCH/install.sh
@@ -1,1 +1,1 @@
-pip install git+https://github.com/janoPig/HROCH.git@e0c22cf2772c599ac9ef62d38e3eccc25a355ed3
+pip install git+https://github.com/janoPig/HROCH.git@9d8fff86723dfede379303719c6189f8fd885eb7
