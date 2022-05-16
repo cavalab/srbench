08a334daa82b7a3943d35a296fdc2c3fc3eaceaa
lacava <williamlacava@gmail.com>
2022-05-16 11:28:28 -0400

    make local ci more flexible

--- local_ci.sh
+++ local_ci.sh
@@ -1,5 +1,7 @@
 SUBNAME=$1
-SUBFOLDER=official_competitors/$SUBNAME 
+SUBFOLDER="$2/$1"
+echo "testing $SUBFOLDER"
+# SUBFOLDER=official_competitors/$SUBNAME 
 SUBENV=srcomp-$SUBNAME 
 # update base env
 mamba env update -n srcomp -f environment.yml 
@@ -8,9 +10,12 @@ mamba env update -n srcomp -f environment.yml
 cd $SUBFOLDER
 pwd
 echo "Installing dependencies for ${SUBNAME}"
+echo "........................................"
 echo "Copying base environment"
-# conda create --name $SUBENV --clone srcomp
+echo "........................................"
+conda create --name $SUBENV --clone srcomp
 echo "Installing conda dependencies"
+echo "........................................"
 mamba env update -n $SUBENV -f environment.yml
 
 eval "$(conda shell.bash hook)"
@@ -18,12 +23,15 @@ conda init bash
 conda activate $SUBENV
 if test -f "install.sh" ; then
 echo "running install.sh..."
+echo "........................................"
 bash install.sh
 else
 echo "::warning::No install.sh file found in ${SUBFOLDER}. Assuming the method is a conda package specified in environment.yml."
 fi
 
 # Copy files and environment
+echo "Copying files and environment to experiment/methods ..."
+echo "........................................"
 cd ../../
 mkdir -p experiment/methods/$SUBNAME
 cp $SUBFOLDER/regressor.py experiment/methods/$SUBNAME/
@@ -38,7 +46,11 @@ conda env export -n $SUBENV > $SUBFOLDER/environment.lock.yml
 cd experiment
 pwd
 ls
+echo "activating conda env $SUBENV..."
+echo "........................................"
 conda activate $SUBENV 
+conda env list 
+conda info 
 python -m pytest -v test_submission.py --ml $SUBNAME
 
 # Store Competitor
