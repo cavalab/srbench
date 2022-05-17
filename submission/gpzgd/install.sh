59e38ca934304d32f46fb6187cb56cd4d10de019
Grant Dick <grant.dick@otago.ac.nz>
2022-05-17 23:42:55 +1200

    Another edit to fix up and hopefully clarify command line path

--- submission/gpzgd/install.sh
+++ submission/gpzgd/install.sh
@@ -1,4 +1,6 @@
 ## compile the CLI executable
 make nuke && make
 
-cp dist/regressor ${CONDA_PREFIX}/bin
+cp dist/regressor ${CONDA_PREFIX}/bin/gpzgd_regressor
+
+chmod +x ${CONDA_PREFIX}/bin/gpzgd_regressor
