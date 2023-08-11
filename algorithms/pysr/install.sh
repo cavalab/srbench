887ce30b9212f35e4bd0279e8488b0fba34fa4b4
lacava <williamlacava@gmail.com>
2023-08-11 15:24:29 -0400

    update ci to try to parallelize

--- algorithms/pysr/install.sh
+++ algorithms/pysr/install.sh
@@ -1,1 +1,32 @@
-python -c 'import pysr; pysr.install()'
+# First, install Julia:
+case "$(uname -sr)" in
+
+   Darwin*)
+     echo 'Installing Julia on Mac OS X'
+     curl -fsSL https://install.julialang.org | sh -s -- -y
+     ;;
+
+   Linux*Microsoft*)
+     echo 'Installing Julia on WSL'  # Windows Subsystem for Linux
+     curl -fsSL https://install.julialang.org | sh -s -- -y
+     ;;
+
+   Linux*)
+     echo 'Installing Julia on Linux'
+     curl -fsSL https://install.julialang.org | sh -s -- -y
+     ;;
+
+   CYGWIN*|MINGW*|MINGW32*|MSYS*)
+     echo 'Installing Julia on MS Windows'
+     winget install julia -s msstore
+     ;;
+
+   *)
+     echo 'OS not detected. Trying to use curl...'
+     curl -fsSL https://install.julialang.org | sh -s -- -y
+     ;;
+esac
+# (architecture detection tip from https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux)
+
+# Then, install Julia dependencies
+python -m pysr install
