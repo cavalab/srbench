8732013c4e22ef12e0a23c2cc72515e4d6edc40a
lacava <williamlacava@gmail.com>
2023-08-16 14:56:47 -0400

    make ITEA install

--- algorithms/itea/install.sh
+++ algorithms/itea/install.sh
@@ -9,4 +9,15 @@ fi
 git clone https://github.com/folivetti/ITEA.git
 
 cd ITEA
-./install_ghcup.sh
+
+# WGL NOTE: this is a temp fix until PR https://github.com/folivetti/ITEA/pull/12 is merged
+# install ghcup
+export BOOTSTRAP_HASKELL_NONINTERACTIVE=1
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | bash
+export PATH=$PATH:~/.ghcup/bin:~/.cabal/bin 
+
+#conda activate srbench
+cabal install
+cp ~/.cabal/bin/itea ./python/
+cd python 
+pip install .
