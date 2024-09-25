3f55d7e3c6c853eb3f97a109320d7092456d65d5
JFKakaJFK <j.koch@student.tudelft.nl>
2024-09-25 11:06:17 +0200

    change to MGPG, try #1

--- algorithms/gpgomea/install.sh
+++ algorithms/gpgomea/install.sh
@@ -3,8 +3,9 @@ if [ -d "gpg" ]; then
     rm -rf gpg
 fi
 
-git clone https://github.com/marcovirgolin/gpg
+git clone https://github.com/matigekunstintelligentie/MultiGPG.git
 
 cd gpg
-git checkout 4598833bbfbeb235af72c8f3393ae72b7eaf8d82
+# TODO update this...
+git checkout 804484f0eff7d12d6ac99e623087dcec3c8b7300
 make
