97e59e66bd491aa7104d849e605115a197265b06
G. Bomarito <geoffrey.f.bomarito@nasa.gov>
2024-10-01 16:52:46 -0400

    updated bingo submission for srbench3

--- algorithms/bingo/install.sh
+++ algorithms/bingo/install.sh
@@ -1,16 +1,7 @@
 # install Bingo
-git clone --recurse-submodules https://github.com/nightdr/bingo.git
+git clone --recurse-submodules https://github.com/nasa/bingo.git
 cd bingo
-
-## checkout srbench bingo
-git checkout tags/srbench_bingo -b srbench_competition
-
-## checkout srbench bingocpp
-cd bingocpp
-git remote add nightdr https://github.com/nightdr/bingocpp.git
-git fetch nightdr
-git checkout tags/srbench_bingocpp -b srbench_competition
-cd ..
+git checkout srbench3
 
 # install
 pip install .
