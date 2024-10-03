87eb4fb50d1ebb2ca10ee13fd991af2d5d221c10
G. Bomarito <geoffrey.f.bomarito@nasa.gov>
2024-10-03 14:29:10 -0400

    fixing bingo version

--- algorithms/bingo/install.sh
+++ algorithms/bingo/install.sh
@@ -1,7 +1,6 @@
 # install Bingo
-git clone --recurse-submodules https://github.com/nasa/bingo.git
+git clone --recurse-submodules --depth 1 --branch v0.4.1.srbench https://github.com/nasa/bingo.git
 cd bingo
-git checkout srbench3
 
 # install
 pip install .
