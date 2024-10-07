1e241786b77296bd267f977ac097a539e93d509a
G. Bomarito <geoffrey.f.bomarito@nasa.gov>
2024-10-07 12:52:45 -0400

    tring to install mpi4py in conda instead of pip

--- algorithms/bingo/install.sh
+++ algorithms/bingo/install.sh
@@ -1,7 +1,3 @@
-# install mpi4py
-apt-get install -y mpich libmpich-dev
-pip install mpi4py>=2.0.0,<4.0
-
 # install Bingo
 git clone --recurse-submodules --depth 1 --branch v0.4.1.srbench https://github.com/nasa/bingo.git
 cd bingo
