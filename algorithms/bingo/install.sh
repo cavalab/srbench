65d1226cc12818b2849343bd20007a9c7ef23ba7
G. Bomarito <geoffrey.f.bomarito@nasa.gov>
2024-10-07 12:20:32 -0400

    moving mpi4py out of environment and into install

--- algorithms/bingo/install.sh
+++ algorithms/bingo/install.sh
@@ -1,5 +1,6 @@
-# install mpi
+# install mpi4py
 apt-get install -y mpich libmpich-dev
+pip install mpi4py>=2.0.0,<4.0
 
 # install Bingo
 git clone --recurse-submodules --depth 1 --branch v0.4.1.srbench https://github.com/nasa/bingo.git
