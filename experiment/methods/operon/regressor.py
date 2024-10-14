f6936b36436be622ee45c6a9bfd1d83981c85166
Alcides Fonseca <me@alcidesfonseca.com>
2024-10-14 17:46:18 +0100

    Reverted Operon regressor

--- experiment/methods/operon/regressor.py
+++ experiment/methods/operon/regressor.py
@@ -26,9 +26,13 @@ def compute_mdl(tree, X, y):
     problem.TrainingRange = Operon.Range(0, X.shape[0])
     target = max(ds.Variables, key=lambda x: x.Index)
     problem.Target = target
-    problem.InputHashes = [ v.Hash for v in sorted(ds.Variables, key=lambda x: x.Index) if v.Hash != target.Hash ]
+    problem.InputHashes = [
+        v.Hash
+        for v in sorted(ds.Variables, key=lambda x: x.Index)
+        if v.Hash != target.Hash
+    ]
     dtable = Operon.DispatchTable()
-    mdl_eval = Operon.MinimumDescriptionLengthEvaluator(problem, dtable, 'gauss')
+    mdl_eval = Operon.MinimumDescriptionLengthEvaluator(problem, dtable, "gauss")
     mdl_eval.Sigma = [compute_sigma(X, y)]
     rng = Operon.RomuTrio(1234)
     ind = Operon.Individual()
@@ -53,13 +57,13 @@ class SympyExprModel(RegressorMixin):
         Constructor which builds a lambda from the expression string using the specified variable names.
 
     """
-    def __init__(self, expr_str, variable_names):
-        self.model          = expr_str
-        self.variable_names = variable_names
-        self.symbols        = [Symbol(x) for x in variable_names]
-        self.best_estimator = lambdify(self.symbols, parse_expr(expr_str))
-        self.is_fitted      = True
 
+    def __init__(self, expr_str, variable_names):
+        self.model = expr_str
+        self.variable_names = variable_names
+        self.symbols = [Symbol(x) for x in variable_names]
+        self.best_estimator = lambdify(self.symbols, parse_expr(expr_str))
+        self.is_fitted = True
 
     def predict(self, X):
         values = X.values if isinstance(X, pd.DataFrame) else X
@@ -77,16 +81,16 @@ class Objective:
         self.params = fixed_params
         self.suggest_callback = suggest_callback
 
-
     def __call__(self, trial):
-        hyper_params = {} if self.suggest_callback is None else self.suggest_callback(trial)
+        hyper_params = (
+            {} if self.suggest_callback is None else self.suggest_callback(trial)
+        )
         params = copy.deepcopy(self.params)
         params.update(hyper_params)
-        params.update({'random_state': self.seed})
+        params.update({"random_state": self.seed})
         self.reg = SymbolicRegressor(**params)
 
         X_, y_ = np.asarray(self.X), np.asarray(self.y)
-
 
         if self.n_folds > 1:
             kf = KFold(n_splits=self.n_folds)
@@ -94,26 +98,35 @@ class Objective:
 
             for train, test in kf.split(X_, y_):
                 X_train, y_train = X_[train, :], y_[train]
-                X_test, y_test   = X_[test, :], y_[test]
+                X_test, y_test = X_[test, :], y_[test]
                 self.reg.fit(X_train, y_train)
-                best = min(self.reg.pareto_front_, key=lambda x: x['minimum_description_length'])
-                score += compute_mdl(best['tree'], X_test, y_test)
+                best = min(
+                    self.reg.pareto_front_,
+                    key=lambda x: x["minimum_description_length"],
+                )
+                score += compute_mdl(best["tree"], X_test, y_test)
 
-            return score/self.n_folds
-
+            return score / self.n_folds
 
         self.reg.fit(X_, y_)
-        return min((x['minimum_description_length'] for x in self.reg.pareto_front_))
-
+        return min((x["minimum_description_length"] for x in self.reg.pareto_front_))
 
     def callback(self, study, trial):
         if self.best_reg is None or study.best_trial == trial:
             self.best_reg = self.reg
 
 
-
 class OperonOptuna(BaseEstimator, RegressorMixin):
-    def __init__(self, name, n_folds=3, n_trials=100, max_time=1,0, fixed_params={}, suggest_callback=None, random_state=None):
+    def __init__(
+        self,
+        name,
+        n_folds=3,
+        n_trials=100,
+        max_time=3600,
+        fixed_params={},
+        suggest_callback=None,
+        random_state=None,
+    ):
         self.name = name
         self.n_folds = n_folds
         self.n_trials = n_trials
@@ -122,69 +135,91 @@ class OperonOptuna(BaseEstimator, RegressorMixin):
         self.fixed_params = fixed_params
         self.suggest_callback = suggest_callback
         self.best_estimator = None  # to hold the best estimator
-        self.best_parameters = None # to hold the best parameters
-
+        self.best_parameters = None  # to hold the best parameters
 
     def fit(self, X, y):
         t0 = time.time()
         sErr = compute_sigma(X, y)
         fixed_params = copy.deepcopy(self.fixed_params)
-        fixed_params.update({'uncertainty': [sErr]})
+        fixed_params.update({"uncertainty": [sErr]})
         t1 = time.time()
-        curr_time = np.ceil(t1-t0)
+        curr_time = np.ceil(t1 - t0)
 
-        objective = Objective(X, y, self.n_folds, fixed_params=fixed_params, suggest_callback=self.suggest_callback, seed=self.random_state)
-        study = optuna.create_study(study_name=self.name, direction='minimize')
-        study.optimize(objective, n_trials=self.n_trials, callbacks=[objective.callback], timeout=(self.max_time - curr_time))
-        self.best_estimator  = objective.best_reg
+        objective = Objective(
+            X,
+            y,
+            self.n_folds,
+            fixed_params=fixed_params,
+            suggest_callback=self.suggest_callback,
+            seed=self.random_state,
+        )
+        study = optuna.create_study(study_name=self.name, direction="minimize")
+        study.optimize(
+            objective,
+            n_trials=self.n_trials,
+            callbacks=[objective.callback],
+            timeout=(self.max_time - curr_time),
+        )
+        self.best_estimator = objective.best_reg
         self.best_parameters = self.best_estimator.get_params()
-        self.best_parameters['completed trials'] = len(study.trials)
+        self.best_parameters["completed trials"] = len(study.trials)
 
         # save the best solution as a SympyExprModel
         best = self.best_estimator
-        names = X.columns.tolist() if isinstance(X, pd.DataFrame) else list(best.variables_.values())
-        model_string = lambda x: best.get_model_string(x, 6, names).replace('^', '**')
+        names = (
+            X.columns.tolist()
+            if isinstance(X, pd.DataFrame)
+            else list(best.variables_.values())
+        )
+        model_string = lambda x: best.get_model_string(x, 6, names).replace("^", "**")
 
         self.best_solution = SympyExprModel(model_string(best.model_), names)
-        self.model         = self.best_solution.model
-        self.population    = [SympyExprModel(model_string(x['tree']), names) for x in best.pareto_front_[:100]]
-
+        self.model = self.best_solution.model
+        self.population = [
+            SympyExprModel(model_string(x["tree"]), names)
+            for x in best.pareto_front_[:100]
+        ]
 
     def predict(self, X):
         return self.best_estimator.predict(X)
 
 
-num_threads = int(os.environ['OMP_NUM_THREADS']) if 'OMP_NUM_THREADS' in os.environ else 1
+num_threads = (
+    int(os.environ["OMP_NUM_THREADS"]) if "OMP_NUM_THREADS" in os.environ else 1
+)
 
 default_params = {
-        'offspring_generator': 'basic',
-        'initialization_method': 'btc',
-        'n_threads': num_threads,
-        'objectives':  ['r2', 'length'],
-        'epsilon': 1e-5,
-        'optimizer_iterations': 1,
-        'random_state': None,
-        'reinserter': 'keep-best',
-        'max_evaluations': int(1e6),
-        'female_selector' : 'tournament',
-        'male_selector': 'tournament',
-        'brood_size': 5,
-        'population_size': 1000,
-        'pool_size': None,
-        'max_time': 300,
-        'model_selection_criterion': 'minimum_description_length',
-    }
+    "offspring_generator": "basic",
+    "initialization_method": "btc",
+    "n_threads": num_threads,
+    "objectives": ["r2", "length"],
+    "epsilon": 1e-5,
+    "optimizer_iterations": 1,
+    "random_state": None,
+    "reinserter": "keep-best",
+    "max_evaluations": int(1e6),
+    "female_selector": "tournament",
+    "male_selector": "tournament",
+    "brood_size": 5,
+    "population_size": 1000,
+    "pool_size": None,
+    "max_time": 300,
+    "model_selection_criterion": "minimum_description_length",
+}
 
 
 def suggest_params(trial):
     return {
-        'allowed_symbols' : trial.suggest_categorical('allowed_symbols', [
-            'add,sub,mul,div,constant,variable',
-            'add,sub,mul,div,sin,cos,constant,variable',
-            'add,sub,mul,div,exp,log,constant,variable',
-            'add,sub,mul,div,sin,cos,exp,log,sqrt,pow,constant,variable'
-        ]),
-        'max_length' : trial.suggest_int('max_length', low=10, high=50, step=10),
+        "allowed_symbols": trial.suggest_categorical(
+            "allowed_symbols",
+            [
+                "add,sub,mul,div,constant,variable",
+                "add,sub,mul,div,sin,cos,constant,variable",
+                "add,sub,mul,div,exp,log,constant,variable",
+                "add,sub,mul,div,sin,cos,exp,log,sqrt,pow,constant,variable",
+            ],
+        ),
+        "max_length": trial.suggest_int("max_length", low=10, high=50, step=10),
     }
 
 
@@ -200,7 +235,13 @@ def get_population(est):
     return [est] if isinstance(est, SympyExprModel) else est.population
 
 
-est = OperonOptuna('operon-optuna', n_folds=1, n_trials=100, fixed_params=default_params, suggest_callback=suggest_params)
+est = OperonOptuna(
+    "operon-optuna",
+    n_folds=1,
+    n_trials=100,
+    fixed_params=default_params,
+    suggest_callback=suggest_params,
+)
 
 ################################################################################
 # Optional Settings
@@ -238,7 +279,4 @@ def pre_train_fn(est, X, y):
 
 
 # pass the function to eval_kwargs
-eval_kwargs = {
-    'pre_train': pre_train_fn,
-    'test_params': {'max_time': 60 }
-}
+eval_kwargs = {"pre_train": pre_train_fn, "test_params": {"max_time": 60}}
