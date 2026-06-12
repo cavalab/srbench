132bdaf71c8ba96e8ec7258cdc1ba6cfbbbcacdd
Guilherme Seidyo Imai Aldeia <guilherme.seidyo@gmail.com>
2026-06-12 13:52:22 -0400

    making curl retry the download before failing

--- algorithms/nesymres/install.sh
+++ algorithms/nesymres/install.sh
@@ -10,4 +10,7 @@ pip install -e src/
 
 # Download pretrained weights
 mkdir -p /srbench_pretrained
-curl -L https://huggingface.co/TommasoBendinelli/NeuralSymbolicRegressionThatScales/resolve/main/100M.ckpt --output srbench_pretrained/nesymres_100M.ckpt
+curl -L --fail --retry 5 \
+     --retry-delay 5 \
+     --continue-at - \
+     -o https://huggingface.co/TommasoBendinelli/NeuralSymbolicRegressionThatScales/resolve/main/100M.ckpt --output srbench_pretrained/nesymres_100M.ckpt
