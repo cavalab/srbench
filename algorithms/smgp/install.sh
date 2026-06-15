1ad3c9f246c932bf48b8870b514346f6df0638db
MichalicekPetr <pemi3943@upce.cz>
2026-06-15 18:02:48 +0200

    Edit of install.sh

--- algorithms/smgp/install.sh
+++ algorithms/smgp/install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+set -ex # Zobrazí každý příkaz, který se spouští
 
-# install smgp
+# 1. Klonujeme do cílové složky
+mkdir -p methods
+rm -rf methods/smgp
 git clone https://github.com/MichalicekPetr/SRBench-SMGPRegressor-Src-Files.git methods/smgp
-cd smgp
 
-# install
+# 2. PŘESNÝ PŘECHOD DO SLOŽKY
+# Musíme vlézt tam, kam jsme to naklonovali!
+cd methods/smgp
+
+# 3. Instalace
 pip install .
-cd ../
+
+# 4. Diagnostika (pro jistotu, že tam setup.py je)
+ls -la setup.py
