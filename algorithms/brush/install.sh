438189b828da636e19fc8d79acfc6acf9a214d33
Guilherme Seidyo Imai Aldeia <guilherme.seidyo@gmail.com>
2026-07-30 15:40:41 -0400

    attempt to fix methods based on c++ and julia

--- algorithms/brush/install.sh
+++ algorithms/brush/install.sh
@@ -1,10 +1,11 @@
-# !/bin/bash
+#!/bin/bash
+set -e
 
 git clone https://github.com/cavalab/brush.git
 cd brush
 
-# fix version
-# git checkout 603a814
+# Keep the source compatible with the pinned Eigen/Boost toolchain.
+git checkout 603a814
 
 # Ensure glog headers are consumed with the expected export/gflags macros.
 export CFLAGS="${CFLAGS} -DGLOG_USE_GLOG_EXPORT -DGLOG_USE_GFLAGS"
