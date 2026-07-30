d3ae329490c00a6f78016a590a4f8dfd4447b372
Guilherme Seidyo Imai Aldeia <guilherme.seidyo@gmail.com>
2026-07-30 16:11:57 -0400

    updating version of fixed dependencies for brush

--- algorithms/brush/install.sh
+++ algorithms/brush/install.sh
@@ -4,11 +4,10 @@ set -e
 git clone https://github.com/cavalab/brush.git
 cd brush
 
-# Keep the source compatible with the pinned Eigen/Boost toolchain.
-git checkout 603a814
-
 # Ensure glog headers are consumed with the expected export/gflags macros.
 export CFLAGS="${CFLAGS} -DGLOG_USE_GLOG_EXPORT -DGLOG_USE_GFLAGS"
 export CXXFLAGS="${CXXFLAGS} -DGLOG_USE_GLOG_EXPORT -DGLOG_USE_GFLAGS"
+export CC="${CC:-clang}"
+export CXX="${CXX:-clang++}"
 
-pip install .
+python -m pip install .
